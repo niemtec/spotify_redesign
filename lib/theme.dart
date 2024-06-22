8be8873c70cb7c41e71eb3f522815d256785a8d7
Jakub Niemiec <niemtec@users.noreply.github.com>
2024-06-22 23:29:57 +0100

    Updated theme with spotify colours

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,52 +3,76 @@ import 'colors.dart'; // Adjust the path to your actual file
 import 'text_styles.dart'; // Adjust the path to your actual file
 
 class AppTheme {
-  static final ThemeData lightTheme = ThemeData(
-    primaryColor: AppColors.primary,
-    accentColor: AppColors.lightPrimary,
-    backgroundColor: AppColors.bgLightMain,
-    scaffoldBackgroundColor: AppColors.bgLightMain,
-    appBarTheme: AppBarTheme(
-      color: AppColors.primary,
-      iconTheme: IconThemeData(color: AppColors.fgPrimary),
-      textTheme: TextTheme(
-        headline6: AppTextStyle.h3.copyWith(color: AppColors.fgPrimary),
-      ),
-    ),
-    textTheme: TextTheme(
-      headline3: AppTextStyle.h3,
-      subtitle1: AppTextStyle.sub,
-      bodyText1: AppTextStyle.p,
-      bodyText2: AppTextStyle.small,
-      headline4: AppTextStyle.h4,
-    ),
-    buttonTheme: ButtonThemeData(
-      buttonColor: AppColors.primary,
-      textTheme: ButtonTextTheme.primary,
-    ),
+  // static const lightColorScheme = ColorScheme(
+  //   brightness: Brightness.light,
+  //   primary: AppColors.primary,
+  //   onPrimary: AppColors.fgPrimary,
+  //   secondary: AppColors.lightPrimary,
+  //   onSecondary: AppColors.fgLightPrimary,
+  //   error: Colors.red,
+  //   onError: Colors.white,
+  //   surfaceBright: AppColors.bgLightMain,
+  //   onSurfaceVariant: AppColors.fgTertiary,
+  //   surface: AppColors.bgPrimary,
+  //   onSurface: AppColors.fgTertiary,
+  // );
+
+  static const darkColorScheme = ColorScheme(
+    brightness: Brightness.dark,
+    primary: Color.fromRGBO(28, 215, 96, 1),
+    onPrimary: Colors.black,
+    secondary: Color.fromRGBO(51, 56, 66, 1),
+    onSecondary: Color.fromRGBO(137, 137, 137, 1),
+    tertiary: Color.fromRGBO(224, 224, 224, 1),
+    onTertiary: Colors.black,
+    error: Colors.red,
+    onError: Colors.black,
+    surfaceDim: AppColors.bgSecondary,
+    onSurfaceVariant: AppColors.fgSecondary,
+    surface: Colors.black,
+    onSurface: Colors.white,
   );
 
+  // static final ThemeData lightTheme = ThemeData(
+  //   colorScheme: lightColorScheme,
+  //   useMaterial3: true,
+  //   textTheme: const TextTheme(
+  //     headlineLarge: AppTextStyle.h3,
+  //     headlineMedium: AppTextStyle.h4,
+  //     headlineSmall: AppTextStyle.sub,
+  //     bodyMedium: AppTextStyle.p,
+  //     bodySmall: AppTextStyle.small,
+  //   ),
+  //   appBarTheme: AppBarTheme(
+  //     backgroundColor: lightColorScheme.primary,
+  //     iconTheme: IconThemeData(color: lightColorScheme.onPrimary),
+  //     titleTextStyle: AppTextStyle.h3.copyWith(color: lightColorScheme.onPrimary),
+  //   ),
+  //   buttonTheme: ButtonThemeData(
+  //     buttonColor: lightColorScheme.primary,
+  //     textTheme: ButtonTextTheme.primary,
+  //   ),
+  // );
+
   static final ThemeData darkTheme = ThemeData(
-    primaryColor: AppColors.primary,
-    accentColor: AppColors.lightPrimary,
-    backgroundColor: AppColors.bgMain,
-    scaffoldBackgroundColor: AppColors.bgMain,
-    appBarTheme: AppBarTheme(
-      color: AppColors.primary,
-      iconTheme: IconThemeData(color: AppColors.fgPrimary),
-      textTheme: TextTheme(
-        headline6: AppTextStyle.h3.copyWith(color: AppColors.fgPrimary),
-      ),
-    ),
+    colorScheme: darkColorScheme,
+    useMaterial3: true,
+    splashFactory: NoSplash.splashFactory,
+    highlightColor: Colors.transparent,
     textTheme: TextTheme(
-      headline3: AppTextStyle.h3.copyWith(color: AppColors.fgPrimary),
-      subtitle1: AppTextStyle.sub.copyWith(color: AppColors.fgSecondary),
-      bodyText1: AppTextStyle.p.copyWith(color: AppColors.fgPrimary),
-      bodyText2: AppTextStyle.small.copyWith(color: AppColors.fgSecondary),
-      headline4: AppTextStyle.h4.copyWith(color: AppColors.fgPrimary),
+      headlineLarge: AppTextStyle.h3.copyWith(color: darkColorScheme.onSurface),
+      headlineSmall: AppTextStyle.sub.copyWith(color: darkColorScheme.onSecondary),
+      bodyMedium: AppTextStyle.p.copyWith(color: darkColorScheme.onSurface),
+      bodySmall: AppTextStyle.small.copyWith(color: darkColorScheme.onSecondary),
+      headlineMedium: AppTextStyle.h4.copyWith(color: darkColorScheme.onSurface),
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: darkColorScheme.primary,
+      iconTheme: IconThemeData(color: darkColorScheme.secondary),
+      titleTextStyle: AppTextStyle.h3.copyWith(color: darkColorScheme.onPrimary),
     ),
     buttonTheme: ButtonThemeData(
-      buttonColor: AppColors.primary,
+      buttonColor: darkColorScheme.primary,
       textTheme: ButtonTextTheme.primary,
     ),
   );
